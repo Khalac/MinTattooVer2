28db253fd0c6a18d85462187e3ead099a9992b81
Khalac <74862194+Khalac@users.noreply.github.com>
2023-08-21 23:27:48 +0700

    add img

--- minver2/src/Components/Artist/Artist.js
+++ minver2/src/Components/Artist/Artist.js
@@ -2,6 +2,25 @@ import React from "react";
 import "./Artist.scss";
 
 function Artist() {
+  function importAll(r) {
+    let images = {};
+    r.keys().forEach((item, index) => {
+      images[item.replace("./", "")] = r(item);
+    });
+    return images;
+  }
+  const images = importAll(
+    require.context("../assets/imgTattoo", false, /\.webp$/)
+  );
+  let imgArr = [];
+  for (let i = 1; i <= 20; ++i) {
+    imgArr.push(images[`${i}.webp`]);
+  }
+
+  const listImg = imgArr.map((img) => {
+    return <img src={img} alt="img" loading="lazy" className="imgTattoos" />;
+  });
+
   return (
     <div className="Artist" id="Artist">
       <div className="ArtistText">Meet Our Artist</div>
@@ -10,6 +29,7 @@ function Artist() {
         <div className="ImgArtist2"></div>
         <div className="ImgArtist3"></div>
       </div>
+      <div className="TattoosPage_img">{listImg}</div>
       <div className="ArtistEnd">
         <div className="ArtistLine"></div>
       </div>
